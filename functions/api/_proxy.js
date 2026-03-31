5291953bb72f3d3cd62c8addef2dd905b73fa5c7
tomer <tomer.konforty@arkhivist.io>
2026-03-31 16:35:14 +0300

    _proxy.js: use only one proxy: proxy6

--- functions/api/_proxy.js
+++ functions/api/_proxy.js
@@ -4,14 +4,15 @@ const OREF_HEADERS = {
 };
 
 const NON_TLV_PROXY_HOSTS = [
-  'https://orefproxy5.oref-map.org',
-  'https://orefproxy7.oref-map.org',
+  'https://orefproxy6.oref-map.org',
+  // 'https://orefproxy5.oref-map.org',
+  // 'https://orefproxy7.oref-map.org',
 ];
 
 // Dedicated pool for TLV traffic so Israeli edge load can be isolated from the
 // general non-TLV proxy pool.
 const TLV_PROXY_HOSTS = [
-//  'https://oreftest.kon40.com',
+ // 'https://oreftest.kon40.com',
   'https://orefproxy6.oref-map.org',
 ];
 
