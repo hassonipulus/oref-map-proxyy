72bcd1d65c30b0fe99b14f4d2a573d484f52a686
hassonipulus <hassonipulus@protonmail.ch>
2026-04-14 01:40:51 +0300

    Update history.js

--- functions/api/history.js
+++ functions/api/history.js
@@ -3,7 +3,7 @@ import { orefProxy } from './_proxy.js';
 export async function onRequestGet(context) {
   return orefProxy(context, {
     target: 'https://www.oref.org.il/warningMessages/alert/History/AlertsHistory.json',
-    redirectSuffix: '/api2/history',
+    redirectSuffix: '/api/history',
     kind: 'history',
   });
 }
